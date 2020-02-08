72f2e4982bf6e4b1e7932429dedb54a676091354
aisukdl <51831489+aisukdl@users.noreply.github.com>
2020-02-08 17:25:43 +0700

    cat meme added
    delete apikey

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
-import cat from './cat.jpg'
+import apikey from './apiKey'
 
 function App() {
+  const [pic, setPic] = useState();
+  const randomCat = () => {
+    fetch(`http://api.giphy.com/v1/gifs/random?api_key=${apikey}&tag=cat`)
+      .then(res => res.json())
+      .then(json => setPic(json.data.image_original_url))
+  }, []);
+
   return (
     <div className="App">
       <h1>Cat</h1>
@@ -11,7 +18,7 @@ function App() {
       <h1>made</h1>
       <h1>your</h1>
       <h1>day</h1>
-      <img src={cat} alt="" />
+      <img src={pic} alt="" />
       <br/>
       <button>GIMME ANOTHER!</button>
     </div>
