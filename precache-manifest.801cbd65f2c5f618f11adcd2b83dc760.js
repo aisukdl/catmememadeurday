33887b6edc5fb27d701c718c9248e545bb4a56cb
aisukdl <51831489+aisukdl@users.noreply.github.com>
2020-02-08 18:21:11 +0700

    Updates

---
+++ precache-manifest.801cbd65f2c5f618f11adcd2b83dc760.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "042071f559c535ee0f3327b00f8d1e35",
+    "url": "./index.html"
+  },
+  {
+    "revision": "29826959e6ea97fd038b",
+    "url": "./static/css/main.d680fba9.chunk.css"
+  },
+  {
+    "revision": "8804bbb1c7e7dbcff9d4",
+    "url": "./static/js/2.f0e53fe0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.f0e53fe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29826959e6ea97fd038b",
+    "url": "./static/js/main.dada241f.chunk.js"
+  },
+  {
+    "revision": "d6ef83612d874ddac148",
+    "url": "./static/js/runtime-main.61eed522.js"
+  }
+]);
